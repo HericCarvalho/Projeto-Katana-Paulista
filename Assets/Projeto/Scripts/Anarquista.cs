a6ecce0ea0a7f3f466d98fade8f3564f5d9855d0
Heric R.C Lima <Hericcarvalho48@gmail.com>
2024-10-08 10:03:49 -0300

    5°Update
    5° Atualização - Criação da mecânica de pulo e melhoria no movimento e cenário -

--- Assets/Projeto/Scripts/Anarquista.cs
+++ Assets/Projeto/Scripts/Anarquista.cs
@@ -1,19 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Anarquista : MonoBehaviour
 {
     [SerializeField] private float Vel;
+    private float jumpTimeCounter;
+    [SerializeField] private float jumpTime;
+    [SerializeField] private float jumpForce;
+
+    private bool isJumping;
 
     [SerializeField] private Rigidbody2D rig;
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Animator animator;
 
+    [SerializeField] private string estado;
+    private string AR = "ar";
+    private string CHAO = "chao";
 
+    
+    
 
     private void FixedUpdate()
     {
         Movimento();
+        Jump();
     }
 
     private void Movimento()
@@ -46,12 +58,53 @@ public class Anarquista : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            this.animator.SetBool("Correndo", true);
 
+            this.animator.SetBool("Correndo", true);
+            Vel = 300;
         }
         else
         {
             this.animator.SetBool("Correndo", false);
+            Vel = 100;
+        }
+    }
+    private void Jump()
+    {        
+    if (Input.GetKeyDown (KeyCode.Space) && estado==CHAO)
+    {
+        isJumping = true;
+        rig.velocity = Vector2.up * jumpForce;
+        jumpTimeCounter = jumpTime;            
+    }
+    
+    if (Input.GetKey(KeyCode.Space) && isJumping==true)
+    {
+        if (jumpTimeCounter>0)
+        {
+            rig.velocity = Vector2.up * jumpForce;
+            jumpTimeCounter -= Time.deltaTime;
+        }
+        else { isJumping = false; }
+    }
+    
+    if (Input.GetKeyUp(KeyCode.Space))
+        isJumping = false;
+    }    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Chão")
+        {
+            estado = CHAO;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (estado == CHAO)
+        {
+            if (collision.gameObject.tag == "Chão")
+                estado = AR;
         }
     }
 }
