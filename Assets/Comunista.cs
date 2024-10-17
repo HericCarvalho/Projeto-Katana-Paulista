c8dd26bfa0618945bb70fca7b6e8cec432da8166
Gabriel Torres <biel.rodrigues7@gmail.com>
2024-10-17 13:51:35 -0300

    Comunista v1.2
    agr quando ele detecta o player, ele persegue.

--- Assets/Comunista.cs
+++ Assets/Comunista.cs
@@ -6,42 +6,51 @@ using UnityEngine;
 
 public class Comunista : MonoBehaviour
 {
-  
+    #region Variaveis
+    public enemy_state CurrentState;
+    public Patrulha_state patrolstate;
+    public PlayerD_state playerDstate;
+    public Charge_state chargeState;
     public Rigidbody2D rb;
     public Transform ledgeDetector;
     //private Animator anim;
     public LayerMask groundlayer, obstacleLayer, playerLayer;
     public float raycastDistance, obstacleDistance, playerDistance;
     public float speed;
-    private bool facingRight = true;
+    public int  facingDirection = 1;
     private bool playerDetected;
     public float DetectionPause;
-    // Start is called before the first frame update
+    public float stateTime; 
+    public float playerDWaitTime = 1;
+    public float chargeTime;
+    public float chargeSpeed;
 
+    #endregion
+    #region Unity callbacks
+    private void Awake()
+    {
+        patrolstate = new Patrulha_state(this, "patrol");
+        playerDstate = new PlayerD_state(this, "player detected");
+        chargeState = new Charge_state(this, "charge");
+        
+        CurrentState = patrolstate;
+        CurrentState.Enter();
+    }
     private void Update()
     {
-        CheckForObstacles();
-        CheckForPlayer();
+        CurrentState .LogicUpdate();
+      
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!playerDetected)
-        {
-            if (facingRight)
-            {
-                rb.velocity = new Vector2(speed, rb.velocity.y);
-            }
-            else
-            {
-                rb.velocity = new Vector2(-speed, rb.velocity.y);
 
-            }
-        }
+        CurrentState.PhysicsUpdate();
+       
     }
 
-    void CheckForObstacles ()
+    public bool CheckForObstacles ()
     {
         RaycastHit2D hit = Physics2D.Raycast(ledgeDetector.position, Vector2.down, raycastDistance, groundlayer);
         RaycastHit2D hitObstacle = Physics2D.Raycast(ledgeDetector.position, Vector2.down, obstacleDistance, obstacleLayer);
@@ -49,44 +58,40 @@ public class Comunista : MonoBehaviour
         if (hit.collider == null || hitObstacle.collider == true)
         {
             Debug.Log("cade o chão");
-            Flip();
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
-    void CheckForPlayer()
+   public bool CheckForPlayer()
     {
-        RaycastHit2D hitplayer = Physics2D.Raycast(ledgeDetector.position, facingRight ? Vector2.right : Vector2.left, playerDistance, playerLayer);
+        RaycastHit2D hitplayer = Physics2D.Raycast(ledgeDetector.position, facingDirection ==1 ? Vector2.right : Vector2.left, playerDistance, playerLayer);
 
         if (hitplayer.collider == true)
         {
-            StartCoroutine(PLayerDetected());
+            return true;
         }
-        else if (playerDetected)
+        else
         {
-            StartCoroutine(PlayerNotDetected());
+            return false;
         }
     }
+    #endregion  
 
-    IEnumerator PLayerDetected()
-    {
-        Debug.Log("player");
-        playerDetected= true;
-        rb.velocity = Vector2.zero; 
-        yield return new WaitForSeconds(DetectionPause);
-    }
-    IEnumerator PlayerNotDetected()
-    {
-        yield return new WaitForSeconds(DetectionPause);
-        playerDetected= false;
-    }
-
-    void Flip()
-    {
-        facingRight =!facingRight;
-        transform.Rotate(0, 180, 0);
-    }
+    #region Outras funções
     private void OnDrawGizmos()
     {
-       Gizmos.DrawRay(ledgeDetector.position, (facingRight ? Vector2.right : Vector2.left) * playerDistance);
+       Gizmos.DrawRay(ledgeDetector.position, (facingDirection ==1 ? Vector2.right : Vector2.left) * playerDistance);
     }
-
+    
+    public void SwitchState(enemy_state newState)
+    {
+        CurrentState.Exit();
+        CurrentState = newState;
+        CurrentState.Enter();
+        stateTime = Time.time;
+    }
+    #endregion
 }
