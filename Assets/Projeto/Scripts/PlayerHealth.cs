06530492c6a6606696afb88acced1f57a4ed2b5c
Gabriel Torres <biel.rodrigues7@gmail.com>
2024-10-22 10:29:30 -0300

    comunista v1.3
    agr ele da dano e um knockback no player

---
+++ Assets/Projeto/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+   public int health;
+
+    public void Damage(float damageAmount)
+    {
+        Debug.Log("megadano");
+        health -= (int)damageAmount;
+    }
+
+    private void Update()
+    {
+        if (health <= 0)
+            Destroy(gameObject);
+    }
+}
